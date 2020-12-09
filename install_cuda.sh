9c97e209f54516dea22adda078e7c39c1c4ca9d0
Rafael Rey <reyarcenegui@gmail.com>
2020-12-09 17:30:32 +0100

    Fix cuda bionic installation

--- install_cuda.sh
+++ install_cuda.sh
@@ -5,13 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 if [ "$release" == "bionic" ]; then
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-    mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+    wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
     
-    wget http://developer.download.nvidia.com/compute/cuda/11.0.1/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.1-450.36.06-1_amd64.deb
-    dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.1-450.36.06-1_amd64.deb
-    
-    apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
-    apt update && apt -yq install cuda
+    sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+    sudo apt-get updatesudo apt-get -y install cuda
 
 elif [ "$release" == "xenial" ]; then
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-ubuntu1604.pin
