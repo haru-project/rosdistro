954aea545a6a88e15acaab9c94e5b50257598c83
Javier Ponce <javichulani@gmail.com>
2020-12-10 10:08:43 +0100

    Fixed typo

--- install_cuda.sh
+++ install_cuda.sh
@@ -11,7 +11,7 @@ if [ "$release" == "bionic" ]; then
     sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
     
     sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
-    sudo apt-get update sudo apt-get -y install cuda
+    sudo apt-get update && sudo apt-get -y install cuda
 
 elif [ "$release" == "xenial" ]; then
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-ubuntu1604.pin
